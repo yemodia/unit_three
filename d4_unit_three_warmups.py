944feaf0881708113b4c824ce1b4d8de5889679e
Yerim <yerim@iMac6.local>
2018-09-28 08:21:15 -0400

    writing document strings

---
+++ d4_unit_three_warmups.py
@@ -0,0 +1,10 @@
+def area_of_rectangle(length, width):
+    """
+    this program is finding the area of a rectangle
+    :param length: long side of the rectangle
+    :param width: short side of rectangle
+    :return: it will return the area in m ** 2
+    """
+
+    area = length * width
+    return area
